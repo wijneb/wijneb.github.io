6741bd53f694af2027709552a0aa8f5f92fa0e29
wijneb <108553941+wijneb@users.noreply.github.com>
2024-06-02 00:28:15 +0200

    Update .initob.sh

--- .initob.sh
+++ .initob.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-myfeh="feh --bg-scale '.background.jpg' "
 mygit="https://raw.githubusercontent.com/wijneb"
 menu=$mygit"/wijneb.github.io/main/menubu.xml"
 myrc=$mygit"/wijneb.github.io/main/rcbu.xml"
@@ -8,7 +7,6 @@ mypic=$mygit"/backgrounds/main/backgrounds/0040.jpg"
 curl -s $menu > .config/openbox/menu.xml
 curl -s $myrc > .config/openbox/rc.xml
 curl -s $mypic > .background.jpg
-echo $myfeh >> .config/openbox/autostart
 openbox --reconfigure
 feh --bg-scale '/home/bw/.background.jpg' 
 # ADD https://raw.githubusercontent.com/wijneb/wijneb.github.io/main/.initob.sh /home/$RDP_USER/
