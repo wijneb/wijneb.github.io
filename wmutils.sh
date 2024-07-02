6b5b0fd561351dd4b0580e06de59b7c57cd8c0d3
wijneb <108553941+wijneb@users.noreply.github.com>
2024-07-02 22:39:11 +0200

    Update wmutils.sh

--- wmutils.sh
+++ wmutils.sh
@@ -5,7 +5,7 @@ MYREPO=https://dl-cdn.alpinelinux.org/alpine/edge/testing
 echo $MYREPO >> /etc/apk/repositories
 apk add wmutils adwaita-icon-theme adw-gtk3 xfce
 mkdir /home/bw/.local/bin
-tar -xzf /mygthb/mybins.tar.gz -C /home/bw/.local/bin
+tar -xzf /mygithb/mybins.tar.gz -C /home/bw/.local/bin
 
 cat <<EOF > .xinitrc
 export PATH="$PATH:/home/bw/.local/bin"
