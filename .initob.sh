684602b7e42e93a236f17440961f03117aff7aa1
wijneb <108553941+wijneb@users.noreply.github.com>
2024-08-28 21:04:41 +0200

    Update .initob.sh

--- .initob.sh
+++ .initob.sh
@@ -12,3 +12,17 @@ openbox --reconfigure
 feh --bg-scale '/home/.background.jpg' 
 # ADD https://raw.githubusercontent.com/wijneb/wijneb.github.io/main/.initob.sh /home/$RDP_USER/
 # sh /home/bw/.initob.sh
+
+README:  rpiallfases6.tar.gz  
+mkdir /home/bw/sel; cd /home/bw/sel
+mygit="https://raw.githubusercontent.com/wijneb/wijneb.github.io/main/rpialpfases6.tar.gz"
+wget -qO- "$mygit" | tar xz -C ./ 
+of:    wget $mygit/rpialpfases6.tar.gz
+       tar -xzvf rpialpfases6.tar.gz 
+(as root) sh alpfase123.sh met reboots tussendoor
+(as root) nano crontab -e   
+0  8 * * * /sbin/reboot  
+0 22 * * * /usr/local/bin/dpmsoff  
+in autostart 
+cd /home/bw/sel
+bwalldias.sh >> mylog &
