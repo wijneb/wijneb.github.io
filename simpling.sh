c64011dcf239f17d61f75f89339a74bf744eb7e7
wijneb <108553941+wijneb@users.noreply.github.com>
2024-11-16 22:16:33 +0100

    Update simpling.sh
    text edit

--- simpling.sh
+++ simpling.sh
@@ -3,7 +3,15 @@
 myvar1=$1
 myvar2=$2
 myvar3="
-This is simpling.sh, two inputs are required, 1: the input file, (note that the input file needs to be converted first with the tolower function) and 2: one of the functions: tags, tagshort, cats, tolower, headers, deduplicate or simplawk. No or wrong input will show this text. Results will show in this terminal and can be redirected to a file using (in the terminal) the redirect construct: > file. Input and output seperators are semicolons. Note that the simplawk output needs the first line awk shebang (#!/usr/bin/awk -f) to be added and at the end the actual functionality {\$11=myvar\"-\"\$6; print \$0} needs to be added. For terminal presentation the pipe \"|\" to the column -ts\";\" bash/awk functionality might be used, also pipe to sort -nr functionality might be used (n is numerical, r is reversed order). Grep functionality might also be used to grep a subset from the output. Here are som instructions that might be helpfull: \n
+\n This is simpling.sh. Two inputs are required, \n 1: the input file, (note that the input file needs to be converted first with the tolower function) and \n 2: one of the functions: tags, tagshort, cats, tolower, headers, deduplicate or simplawk. \n No or wrong input will show this text. \n Results will show in this terminal and can be redirected to a file using the bash redirect construct: > file. \n Input and output seperators are semicolons. \n Note that the simplawk output needs the first line awk shebang (#!/usr/bin/awk -f) to be added \n and at the end the actual functionality {\$11=myvar\"-\"\$6; print \$0} needs to be added. \n For terminal presentation the pipe \"|\" to the column -ts\";\" bash/awk functionality might be used, \n also pipe to sort -nr functionality might be used (n is numerical, r is reversed order). \n Grep functionality might also be used to grep a subset from the output. \n Here are some instructions that might be helpfull: \n
+sh simpling.sh \n
+sh simpling.sh ING2022e.csv headers \n
+sh simpling.sh ING2022e.csv tolower > L_ING2022.csv \n
+sh simpling.sh L_ING2022.csv tags > L_ING2022withTags.csv \n
+sh simpling.sh L_ING2022.csv cats | sort -n | column -ts\";\" \n
+sh simpling.sh L_ING2022.csv tagshort | grep belasting-af \n
+sh simpling.sh L_ING2022.csv tagshort | sort | column -ts\";\" | grep bij
+"
 sh simpling.sh \n
 sh simpling.sh ING2022e.csv headers \n
 sh simpling.sh ING2022e.csv tolower > L_ING2022.csv \n
