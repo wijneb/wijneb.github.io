d75f60246fad1bd755cd021be1f237318c198271
wijneb <108553941+wijneb@users.noreply.github.com>
2024-11-24 23:48:15 +0100

    Update simpling.sh
    awk file function added

--- simpling.sh
+++ simpling.sh
@@ -5,8 +5,9 @@ myvar2=$2
 myvar3="
 \n This is simpling.sh. Two inputs are required, \n 
 1: the input file \n 2: one of the functions below: \n 
-(dedup, simplawk, headers, tags, tagshort or cats). \n 
-All functions use lowercase and simplawk deduplicates. \n 
+(dedup, awkfile, tags, headers, cats, tagshort, or shotags). \n 
+All functions use lowercase and mytags deduplicates. \n 
+The simple file needs to be in the folder (matching key;value pairs). \n
 No or wrong input will show this text. \n 
 Results will show in this terminal and can be redirected \n 
 to a file using the redirect construct: > file. \n 
@@ -21,36 +22,37 @@ Here are some other example instructions that might be helpfull: \n
 (simple and ING2022e.csv are example input files) \n\n
 sh simpling.sh \n
 sh simpling.sh simple dedup \n
-sh simpling.sh simple simplawk \n
+sh simpling.sh simple awkfile \n
 sh simpling.sh ING2022e.csv tags \n
 sh simpling.sh ING2022e.csv headers \n
+sh simpling.sh ING2022e.csv shotags \n
 sh simpling.sh ING2022e.csv cats | sort -n | column -ts\";\" \n
 sh simpling.sh ING2022e.csv tagshort | sort -n | column -ts\";\" \n
 sh simpling.sh ING2022e.csv tagshort | sort | column -ts\";\" | grep bij \n
 "
 
 inpt()   { cat $myvar1 ; }
-mytags() { awk -f LUsimple.awk ; }
 dedupl() { awk '!visited[$0]++' ; }
+shotags(){ awk '{print $11}' FS=";" ; }
 tolower(){ awk '{print tolower($0)}' ; }
-short()  { awk 'NR>1{print $11,$7,$1,$2}' FS=";" OFS=";" ; }
+tagshrt(){ awk 'NR>1{print $11,$7,$1,$2}' FS=";" OFS=";" ; }
 simpl()  { awk '{print "/"$1"/{myvar=\""$2"\"}"}' FS=";" ; }
 header() { awk 'NR==1 { for (i=1; i<NF+1; i++) print "$"i, $i; exit }'  FS=";" ; }
-sawk()   { mystt='#!/usr/bin/awk -f \n' ; myend='\n{$11=myvar"-"$6; print $0}' 
-           awk -v mystt="$mystt" -v myend="$myend" \
-               'BEGIN{print mystt} {print} END{print myend}' FS=";" OFS=";" ; }
-catcalc(){ awk 'NR>1 { myarr[$11]+=$7 
-                if($6=="af") totaf+=$7; else totbij+=$7 }
-                END{ for (key in myarr) print myarr[key], key  
-                     print totaf, "totaal-af" 
-                     print totbij, "totaal-bij" } ' FS=";" OFS=";" ; }
+catcalc(){ awk -v taf='total-af' -v tbij='totaal-bij' ' 
+           NR>1{ cats[$11]+=$7; if($6=="af") cats[taf]+=$7; else cats[tbij]+=$7 } 
+           END{ for (key in cats) print cats[key],key }' FS=";" OFS=";" ; }
+awkfile(){ echo "awk '{myvar=\"diverse\"}" 
+           echo "$( cat simple | tolower | dedupl | simpl )"
+           echo "{\$11=myvar\"-\"\$6; print \$0} ' FS=';' OFS=';' " ; }
+mytags() { eval "$(awkfile)" ; }
 
 case $myvar2 in
   headers)  inpt | tolower | header ;;
+  dedup)    inpt | tolower | dedupl ;;
   tags)     inpt | tolower | mytags ;;
   cats)     inpt | tolower | mytags | catcalc ;;
-  tagshort) inpt | tolower | mytags | short ;;
-  simplawk) inpt | tolower | dedupl | simpl | sawk ;;
-  dedup)    inpt | tolower | dedup1 ;;
+  tagshort) inpt | tolower | mytags | tagshrt ;;
+  shotags)  inpt | tolower | mytags | shotags ;;
+  awkfile)  awkfile ;;
   *) echo $myvar3 ;;
 esac
